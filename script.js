9c9fdde32ed59c0b78703b8618f05bf3e4cc9c80
Hari Prasad <hariprasadxx121@gmail.com>
2025-09-29 02:12:34 +0530

    generate the grid

---
+++ script.js
@@ -0,0 +1,9 @@
+// generate the grid 
+
+const container = document.querySelector(".container");
+
+for(let i=0; i<256; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+}
